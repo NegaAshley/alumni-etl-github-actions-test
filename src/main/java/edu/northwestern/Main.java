1bb74b5cf965a3063123ad19fc752913cf3ef716
NegaAshley <ashley.xu@northwestern.edu>
2023-11-17 15:02:30 -0600

    Initial commit

---
+++ src/main/java/edu/northwestern/Main.java
@@ -0,0 +1,14 @@
+package edu.northwestern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
